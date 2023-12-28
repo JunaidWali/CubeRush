89558746f8d36da808dd501bf3d1b3c094515508
JunaidWali <56029841+JunaidWali@users.noreply.github.com>
2023-12-28 05:50:37 -0500

    Created Pause Menu UI and implemented functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,83 +4,109 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-	public enum GameMode { SinglePlayer, MultiPlayer }
-	public static GameMode CurrentGameMode { get; private set; }
+    public enum GameMode { SinglePlayer, MultiPlayer }
+    public static GameMode CurrentGameMode { get; private set; }
 
-	public static void SetGameMode(GameMode mode)
-	{
-		CurrentGameMode = mode;
-	}
+    private static bool isPaused = false;
 
-	public static void StartGame()
-	{
-		string prefix = CurrentGameMode == GameMode.SinglePlayer ? "SP" : "MP";
+    public static bool GetIsPaused()
+    {
+        return isPaused;
+    }
 
-		for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
-		{
-			string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
-			string sceneName = System.IO.Path.GetFileNameWithoutExtension(scenePath);
+    public static void SetIsPaused(bool value)
+    {
+        isPaused = value;
+    }
 
-			if (sceneName.StartsWith(prefix))
-			{
-				LoadLevel(sceneName);
-				break;
-			}
-		}
-	}
+    public static void SetGameMode(GameMode mode)
+    {
+        CurrentGameMode = mode;
+    }
 
-	public static void LoadNextLevel()
-	{
-		int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
-		string nextScenePath = SceneUtility.GetScenePathByBuildIndex(nextSceneIndex);
-		string nextSceneName = System.IO.Path.GetFileNameWithoutExtension(nextScenePath);
+    public static void StartGame()
+    {
+        string prefix = CurrentGameMode == GameMode.SinglePlayer ? "SP" : "MP";
 
-		string prefix = CurrentGameMode == GameMode.SinglePlayer ? "SP" : "MP";
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+            string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+            string sceneName = System.IO.Path.GetFileNameWithoutExtension(scenePath);
 
-		if (nextSceneName.StartsWith(prefix))
-		{
-			LoadLevel(nextSceneName);
-		}
-		else
-		{
-			LoadGameOver();
-		}
-	}
+            if (sceneName.StartsWith(prefix))
+            {
+                LoadLevel(sceneName);
+                break;
+            }
+        }
+    }
 
-	public static void LoadMainMenu()
-	{
-		SceneManager.LoadScene("Main_Menu");
-	}
 
-	public static void LoadGameOver()
-	{
-		SceneManager.LoadScene("Game_Over");
-	}
 
-	public static void LoadLevel(string levelName)
-	{
-		SceneManager.LoadScene(levelName);
-		SceneManager.LoadScene("Lighting", LoadSceneMode.Additive);
-	}
+    public static void LoadNextLevel()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        string nextScenePath = SceneUtility.GetScenePathByBuildIndex(nextSceneIndex);
+        string nextSceneName = System.IO.Path.GetFileNameWithoutExtension(nextScenePath);
 
-	public static void PauseGame()
-	{
-		Time.timeScale = 0f;
-	}
+        string prefix = CurrentGameMode == GameMode.SinglePlayer ? "SP" : "MP";
 
-	public static void ResumeGame()
-	{
-		Time.timeScale = 1f;
-	}
+        if (nextSceneName.StartsWith(prefix))
+        {
+            LoadLevel(nextSceneName);
+        }
+        else
+        {
+            LoadGameOver();
+        }
+    }
 
-	public static void RestartLevel()
-	{
-		string activeSceneName = SceneManager.GetActiveScene().name;
-		LoadLevel(activeSceneName);
-	}
+    public static void LoadMainMenu()
+    {
+        ResumeGame();
+        SceneManager.LoadScene("Main_Menu");
+    }
 
-	public static void QuitGame()
-	{
-		Application.Quit();
-	}
+    public static void LoadGameOver()
+    {
+        SceneManager.LoadScene("Game_Over");
+    }
+
+    public static void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+        SceneManager.LoadScene("Lighting", LoadSceneMode.Additive);
+    }
+
+    public static void PauseGame()
+    {
+        // Load the pause menu scene additively
+        SceneManager.LoadScene("Pause_Menu", LoadSceneMode.Additive);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public static void ResumeGame()
+    {
+        // Unload the pause menu scene
+        if (SceneManager.GetSceneByName("Pause_Menu").isLoaded)
+        {
+            SceneManager.UnloadSceneAsync("Pause_Menu");
+            Time.timeScale = 1f;
+            isPaused = false;
+        }
+    }
+
+
+    public static void RestartLevel()
+    {
+        ResumeGame();
+        string activeSceneName = SceneManager.GetActiveScene().name;
+        LoadLevel(activeSceneName);
+    }
+
+    public static void QuitGame()
+    {
+        Application.Quit();
+    }
 }
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class PlayerControls : PlayerInfo
 {
-	void Update()
+    
+    void Update()
 	{
 		if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow)) // If the player is pressing the "d" key or right arrow key
 		{
@@ -21,6 +22,18 @@ public class PlayerControls : PlayerInfo
 				jumpRequest = true;
 			}
 		}
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameManager.GetIsPaused() == true)
+            {
+                GameManager.ResumeGame();
+            }
+            else
+            {
+                GameManager.PauseGame();
+            }
+        }
 
 	}
 }
