ec7f75b3ac9e19db176c99822f125e0ebe850b90
Zubair Shibly <zubairshibly08@gmail.com>
2023-12-28 23:23:05 -0500

    lighting

--- Assets/UI Toolkit/Panels/GameOverScreen.cs
+++ Assets/UI Toolkit/Panels/GameOverScreen.cs
@@ -6,7 +6,7 @@ public class GameOverScreen : MonoBehaviour
     private void Awake()
     {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
-        root.Q<Button>("Main-Menu").clicked += () => { GameManager.LoadMainMenu(); };
-        root.Q<Button>("Quit-Game").clicked += () => { GameManager.QuitGame(); };
+        root.Q<Button>("Main-Menu").clicked += () => GameManager.LoadMainMenu();
+        root.Q<Button>("Quit-Game").clicked += () => GameManager.QuitGame();
     }
 }
