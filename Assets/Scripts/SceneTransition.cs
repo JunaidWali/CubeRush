f6181bffae5f862696f65d673fb24851a27264b5
JunaidWali <56029841+JunaidWali@users.noreply.github.com>
2024-01-06 01:58:46 -0500

    Applied fade transition between scenes

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransition : MonoBehaviour
+{
+    public static SceneTransition Instance { get; private set; }
+
+    [SerializeField] public Animator transition;
+    private float transitionTime = 1f;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            //DontDestroyOnLoad(gameObject); // Optionally make this object persist across scenes
+        }
+    }
+
+    public IEnumerator StartTransition()
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+    }
+}
+
--- Assets/UI Toolkit/Panels/GameOverScreen.cs
+++ Assets/UI Toolkit/Panels/GameOverScreen.cs
@@ -6,7 +6,11 @@ public class GameOverScreen : MonoBehaviour
     private void Awake()
     {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
-        root.Q<Button>("Main-Menu").clicked += () => GameManager.LoadMainMenu();
+        root.Q<Button>("Main-Menu").clicked += () =>
+        {
+            StartCoroutine(SceneTransition.Instance.StartTransition());
+            GameManager.LoadMainMenu();
+        };
         root.Q<Button>("Quit-Game").clicked += () => GameManager.QuitGame();
     }
 }
